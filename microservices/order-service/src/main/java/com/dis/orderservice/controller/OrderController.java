8d0e65daefc3bc30caf0606feb9c0c0d30d7e709
bojananeskovic <bojananeskovic007@gmail.com>
2023-08-20 17:16:39 +0200

    Created Order service

---
+++ microservices/order-service/src/main/java/com/dis/orderservice/controller/OrderController.java
@@ -0,0 +1,24 @@
+package com.dis.orderservice.controller;
+
+import com.dis.orderservice.dto.OrderRequest;
+import com.dis.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/order")
+@RequiredArgsConstructor
+@Slf4j
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
+        orderService.placeOrder(orderRequest);
+        return "Order Placed Successfully";
+    }
+}
